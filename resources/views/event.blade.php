78573493d4988b2123e9708bf6fadc5790cb01bc
mo <melianiplus@gmail.com>
2023-06-14 04:58:41 +0100

    adding icon support

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -2,11 +2,16 @@
     @if($eventClickEnabled)
         wire:click.stop="onEventClick('{{ $event['id']  }}')"
     @endif
-    class="bg-white rounded-lg border py-2 px-3 shadow-md cursor-pointer">
+    class="{{ $event['bg_color'] }} rounded-lg border py-2 px-3 shadow-md cursor-pointer">
 
     <p class="text-sm font-medium">
         {!! $event['title'] !!}
     </p>
+    {{-- Adding icon support // need to be added to constructor--}}
+    <div class="float-right">
+    {!! $event['icons'] ?? '' !!}
+    </div>
+    {{-- end icon --}}
     <div class="text-xs">
     {{-- <p class="mt-2 text-xs"> --}}
         {!! $event['description'] ?? 'No description' !!}
