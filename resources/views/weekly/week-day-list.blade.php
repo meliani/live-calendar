94631677e991a982ba6b0e2000f686bd5d282262
mo <melianiplus@gmail.com>
2023-06-16 05:41:11 +0100

    updated default week view and added a grid view

---
+++ resources/views/weekly/week-day-list.blade.php
@@ -0,0 +1,51 @@
+<div
+    ondragenter="onLiveCalendarEventDragEnter(event, '{{ $componentId }}', '{{ $day }}', '{{ $dragAndDropClasses }}');"
+    ondragleave="onLiveCalendarEventDragLeave(event, '{{ $componentId }}', '{{ $day }}', '{{ $dragAndDropClasses }}');"
+    ondragover="onLiveCalendarEventDragOver(event);"
+    ondrop="onLiveCalendarEventDrop(event, '{{ $componentId }}', '{{ $day }}', {{ $day->year }}, {{ $day->month }}, {{ $day->day }}, '{{ $dragAndDropClasses }}');"
+    class="flex-1 h-40 lg:h-48 border border-gray-200 -mt-px -ml-px"
+    style="min-width: 10rem;">
+
+    {{-- Wrapper for Drag and Drop --}}
+    <div
+        class="w-full h-full"
+        id="{{ $componentId }}-{{ $day }}">
+
+        <div
+            @if($dayClickEnabled)
+                wire:click="onDayClick({{ $day->year }}, {{ $day->month }}, {{ $day->day }})"
+            @endif
+            class="w-full h-full p-2 {{ $dayInMonth ? $isToday ? 'bg-yellow-100' : ' bg-white ' : 'bg-gray-100' }} flex flex-col">
+
+            {{-- Number of Day --}}
+            <div class="flex items-center">
+                <p class="text-sm {{ $dayInMonth ? ' font-medium ' : '' }}">
+                    {{ $day->formatLocalized('%A') }} {{ $day->format('d') }}
+                </p>
+                <p class="text-xs text-gray-600 ml-4">
+                    @if($events->isNotEmpty())
+                        {{ $events->count() }} {{ Str::plural('event', $events->count()) }}
+                    @endif
+                </p>
+            </div>
+
+            {{-- Events --}}
+            <div class="p-2 my-2 flex-1 overflow-y-auto">
+                <div class="grid grid-cols-1 grid-flow-row gap-2">
+                    @foreach($events as $event)
+                        <div
+                            @if($dragAndDropEnabled)
+                                draggable="true"
+                            @endif
+                            ondragstart="onLiveCalendarEventDragStart(event, '{{ $event['id'] }}')">
+                            @include($eventView, [
+                                'event' => $event,
+                            ])
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
--- resources/views/weekly/week-day.blade.php
+++ resources/views/weekly/week-day.blade.php
@@ -1,39 +1,51 @@
-<div class="flex-1 h-40 lg:h-48 border border-gray-200 -mt-px -ml-px" style="min-width: 10rem;"
-    ondragenter="onLiveCalendarEventDragEnter(event, '{{ $componentId }}', '{{ $day }}', '{{ $dragAndDropClasses }}');"
-    ondragleave="onLiveCalendarEventDragLeave(event, '{{ $componentId }}', '{{ $day }}', '{{ $dragAndDropClasses }}');"
-    ondragover="onLiveCalendarEventDragOver(event);"
-    ondrop="onLiveCalendarEventDrop(event, '{{ $componentId }}', '{{ $day }}', {{ $day->year }}, {{ $day->month }}, {{ $day->day }}, '{{ $dragAndDropClasses }}');">
+<div
+    x-data="{ isDragging: false }"
+    x-on:dragenter="isDragging = true"
+    x-on:dragleave="isDragging = false"
+    x-on:dragover="event.preventDefault()"
+    x-on:drop="isDragging = false; $wire.onLiveCalendarEventDrop(event, '{{ $componentId }}', '{{ $day }}', {{ $day->year }}, {{ $day->month }}, {{ $day->day }}, '{{ $dragAndDropClasses }}');"
+    class="flex-1 h-40 lg:h-48 border border-gray-200 -mt-px -ml-px"
+    style="min-width: 10rem;"
+    :class="{ 'bg-yellow-100': {{ $dayInMonth }} && {{ $isToday }}, 'bg-gray-100': !{{ $dayInMonth }} }"
+    :style="{ 'background-color': isDragging ? '#f7fafc' : '' }">
 
-    {{-- Wrapper for Drag and Drop --}}
-    <div class="w-full h-full" id="{{ $componentId }}-{{ $day }}">
+    <!-- Wrapper for Drag and Drop -->
+    <div
+        class="w-full h-full"
+        id="{{ $componentId }}-{{ $day }}">
 
-        <div class="w-full h-full p-2 {{ $dayInMonth ? ($isToday ? 'bg-yellow-100' : ' bg-white ') : 'bg-gray-100' }} flex flex-col"
-            @if ($dayClickEnabled) wire:click="onDayClick({{ $day->year }}, {{ $day->month }}, {{ $day->day }})" @endif>
+        <div
+            @if($dayClickEnabled)
+                x-on:click="$wire.onDayClick({{ $day->year }}, {{ $day->month }}, {{ $day->day }})"
+            @endif
+            class="w-full h-full p-2 flex flex-col">
 
-            {{-- Number of Day --}}
+            <!-- Number of Day -->
             <div class="flex items-center">
-                <p class="text-sm {{ $dayInMonth ? ' font-medium ' : '' }}">
+                <p class="text-sm {{ $dayInMonth ? 'font-medium' : '' }}">
                     {{ $day->formatLocalized('%A') }} {{ $day->format('d') }}
                 </p>
                 <p class="text-xs text-gray-600 ml-4">
-                    @if ($events->isNotEmpty())
+                    @if($events->isNotEmpty())
                         {{ $events->count() }} {{ Str::plural('event', $events->count()) }}
                     @endif
                 </p>
             </div>
 
-            {{-- Events --}}
-            <div class="p-2 my-2 flex-1 overflow-y-auto">
-                <div class="grid grid-cols-1 grid-flow-row gap-2">
-                    @foreach ($events as $event)
-                        <div @if ($dragAndDropEnabled) draggable="true" @endif
-                            ondragstart="onLiveCalendarEventDragStart(event, '{{ $event['id'] }}')">
-                            @include($eventView, [
-                                'event' => $event,
-                            ])
-                        </div>
-                    @endforeach
-                </div>
+            <!-- Events -->
+            <div class="p-2 my-2 flex-1 overflow-y-auto grid gap-2 lg:grid-cols-3 xl:grid-cols-3">
+                @foreach($events as $event)
+                    <div
+                        @if($dragAndDropEnabled)
+                            draggable="true"
+                            x-on:dragstart="$wire.onLiveCalendarEventDragStart(event, '{{ $event['id'] }}')"
+                        @endif
+                        class="lg:w-full xl:w-1/3">
+                        @include($eventView, [
+                            'event' => $event,
+                        ])
+                    </div>
+                @endforeach
             </div>
 
         </div>
